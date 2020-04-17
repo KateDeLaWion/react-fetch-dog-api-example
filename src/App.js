d99c14fd5b9b2d7ad634cf9321a30b2bd884bc4e
Student101 <student101@PSAdmins-Air-3.home>
2020-04-17 12:16:08 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import DogImage from './DogImage';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+constructor(props) {
+  super(props)
+
+  this.state = {
+     dogImages: []
+  }
+}
+
+componentDidMount() {
+  fetch("https://dog.ceo/api/breeds/image/random/20")
+  .then(res => res.json())
+  .then((data) => {
+    this.setState({ dogImages: data.message })
+  })
+}
+
+render() {
+  const dogList = this.state.dogImages.map((url, i) => {
+    return <DogImage key={i} url={url}/>
+  })
+
+  return (
+    <div className="App">
+      <h1>Dog Images</h1>
+      {dogList}
+    </div>
+  );
+}
+
+
+
+}
+
+export default App;
+
+
+
+
---
+++ src/DogImage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DogImage(props) {
+    return (
+        <div>
+            <img src={props.url}/>
+        </div>
+    )
+}
